1d9ed96c076d68ea9d497928f35b40c9e96450fe
Akshay Kamble <kamblea635@gmail.com>
2022-02-08 15:19:47 +0530

    feat: add sidebar for responsiveness

--- product-page/product.js
+++ product-page/product.js
@@ -7,21 +7,21 @@ const sortScreen = document.querySelector(".sort-layout-mob");
 btnFilter.addEventListener("click", () => {
   if (filterScreen.style.display === "none") {
     filterScreen.style.display = "flex";
+    sortScreen.style.display = "none";
     body.classList.add("noscroll");
   } else {
     filterScreen.style.display = "none";
     body.classList.remove("noscroll");
-    sortScreen.style.display = "none";
   }
 });
 
 btnSort.addEventListener("click", () => {
   if (sortScreen.style.display === "none") {
     sortScreen.style.display = "flex";
+    filterScreen.style.display = "none";
     body.classList.add("noscroll");
   } else {
     sortScreen.style.display = "none";
     body.classList.remove("noscroll");
-    filterScreen.style.display = "none";
   }
 });
